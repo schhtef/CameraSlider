6f7a3ff293ea8fbecc50b235f42e0a079094e17c
bassyface <bichlmaier.stef@gmail.com>
2020-11-19 16:45:21 -0800

    Adding Comments

--- CameraSlider.ino
+++ CameraSlider.ino
@@ -1,26 +1,41 @@
+/*
+ * Camera Actuation Control Software V 1.0 
+ * Authored by Stefan Bichlmaier
+ * V00852866
+ * 
+ * Description:
+ * This software provides an easy way to program camera movements on a motorized camera slider.
+ * It creates an interface between a set of stepper motors and their standard, 2 wire drivers through an LCD screen and rotary encoder
+ * 
+ * Features:
+ * -Interrupt-driven rotary encoder and push button input for menu navigation
+ * -Navigatable menu using the ArduinoMenu library
+ * -Stepper motor enable/disable
+ * -Stepper motor homing (finding the desired zero position)
+ * -Simplified timelapse mode
+ *  -Contains parameters which are used to calculate the timing and quantity of photos to be taken, as well as the distance travelled
+ *  -Triggers the release of a DSLR camera by pulsing an infra-red LED
+ *  
+ * Future plans:
+ * -Second axis motor and control
+ * -Code for calculating camera rotation during timelapse
+ * -Before adding more movement modes, generalize the movement struct to minimize code repeats
+*/
 #include <Arduino.h>
-
-/********************
-  Sept. 2014 Rui Azevedo - ruihfazevedo(@rrob@)gmail.com
-  menu output to standard arduino LCD (LiquidCrystal)
-  output: LCD
-  input: encoder and Serial
-  www.r-site.net
-***/
 #include <Wire.h>
-#include <menu.h>
+#include <menu.h> //https://github.com/neu-rah/ArduinoMenu used for menu creation and navigation
 #include <menuIO/liquidCrystalOut.h>
 #include <menuIO/serialOut.h>
 #include <menuIO/serialIn.h>
 #include <menuIO/encoderIn.h>
 #include <menuIO/keyIn.h>
 #include <menuIO/chainStream.h>
-#include <AccelStepper.h>
+#include <AccelStepper.h> //https://www.airspayce.com/mikem/arduino/AccelStepper/ used as a non blocking, open loop stepper motor controller
 #include "PinMap.h"
 
 using namespace Menu;
 
-AccelStepper stepperX(AccelStepper::DRIVER, X_STEP, X_DIR);
+AccelStepper stepperX(AccelStepper::DRIVER, X_STEP, X_DIR); //Initialize X axis stepper object
 
 //--------------------------------------------------------LCD--------------------------------------------------------
 #define LCD_RS 12
@@ -28,75 +43,118 @@ AccelStepper stepperX(AccelStepper::DRIVER, X_STEP, X_DIR);
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 //--------------------------------------------------------LCD--------------------------------------------------------
 
-//--------------------------------------------------------Encoder--------------------------------------------------------
-#define ENC_A 3
-#define ENC_B 2
-#define ENC_PB 4
-#define LEDPIN 13
-
-
+//--------------------------------------------------------Encoder----------------------------------------------------
+//Create serial streams from encoder quadrature outputs and push button
 encoderIn<ENC_A, ENC_B> encoder; //simple quad encoder driver
-encoderInStream<ENC_A, ENC_B> encStream(encoder, 4); // simple quad encoder fake Stream
+encoderInStream<ENC_A, ENC_B> encStream(encoder, 4); // simple quad encoder Stream
 
 //a keyboard with only one key as the encoder button
 keyMap encBtn_map[] = {{ -ENC_PB, defaultNavCodes[enterCmd].ch}}; //negative pin numbers use internal pull-up, this is on when low
 keyIn<1> encButton(encBtn_map);//1 is the number of keys
 
-//input from the encoder + encoder button + serial
+//link input streams into one for menu input
 serialIn serial(Serial);
 menuIn* inputsList[] = {&encStream, &encButton, &serial};
 chainStream<3> in(inputsList);//3 is the number of inputs
-//--------------------------------------------------------Encoder--------------------------------------------------------
+//--------------------------------------------------------Encoder-----------------------------------------------------
 
 //--------------------------------------------------------Movement Modes--------------------------------------------------------
 
-enum ModeStates {
+enum ModeStates {//keep track of which mode the system is in to determine which update functions to call in the event loop
   MENUSTATE = 0,
   TIMELAPSE = 1
 };
 
 ModeStates cameraMode = MENUSTATE;
 
+//timelapse struct contains the variables and functions necessary for deriving camera movements for a timelapse
+//TODO add rotation calculations and second motor control
 struct timelapse{
-  int duration;
-  int clipLength;
-  int framesPerSecond;
-  int distanceX;
+  //variables set from the menu
+  int duration; //duration of the timelapse in real time
+  int clipLength; //duration of the resulting clip (ie. a 4 hour duration resulting in a 10 second clipLength)
+  int framesPerSecond; //frame rate of the resulting clip
+  int distanceX; //distance in X travelled in mm 
+
+  //helper variables
   int currentShot;
   unsigned long timeAtLastShot;
   
-  int getNumFrames(void){
+  int getNumFrames(void){//calculate the number of photos which will be taken through the timelapse
     return clipLength * framesPerSecond;
   }
-  int getTimeToShot(){
+  int getTimeToShot(){//calculate the time between each shot
     return duration/getNumFrames();
   }
-  long getDistanceToShot(){
+  long getDistanceToShot(){//calculate the distance in X between each shot
     return (long)((float)distanceX/(float)(0.009*getNumFrames())); 
   }
   
-  void updateTimelapse(unsigned long currentTime){//all other movements caused by this function
-    if(this->currentShot == this->getNumFrames()) cameraMode = MENUSTATE; //reached end of sequence, stop commanding movements
-    if((currentTime - this->timeAtLastShot) >= this->getTimeToShot()*1000){ //if we have50 reached or passed the time between shots, command next movement
-    //perform shutter and movement logic
-    Serial.println("Shutter released!");
-    this->currentShot++;
-    stepperX.move(this->getDistanceToShot()); //move to next shot relative to current position
-    this->timeAtLastShot = millis();
+  //takePhoto() function from https://www.christidis.info/index.php/personal-projects/arduino-nikon-infrared-command-code
+  //Creates the sequence of timed LED pulses to trigger a Nikon camera's shutter release
+  //this is a blocking function, but it only takes ~50ms to execute. In most cases, the carriage will be stopped for much longer than this
+  void takePhoto(void) {
+    int i;
+    for (i = 0; i < 76; i++) {
+      digitalWrite(IR_LED, HIGH);
+      delayMicroseconds(7);
+      digitalWrite(IR_LED, LOW);
+      delayMicroseconds(7);
+    }
+    delay(27);
+    delayMicroseconds(810);
+    for (i = 0; i < 16; i++) {
+      digitalWrite(IR_LED, HIGH);
+      delayMicroseconds(7);
+      digitalWrite(IR_LED, LOW);
+      delayMicroseconds(7);
+    }
+    delayMicroseconds(1540);
+    for (i = 0; i < 16; i++) {
+      digitalWrite(IR_LED, HIGH);
+      delayMicroseconds(7);
+      digitalWrite(IR_LED, LOW);
+      delayMicroseconds(7);
+    }
+    delayMicroseconds(3545);
+    for (i = 0; i < 16; i++) {
+      digitalWrite(IR_LED, HIGH);
+      delayMicroseconds(7);
+      digitalWrite(IR_LED, LOW);
+      delayMicroseconds(7);
     }
   }
-  void updateTimelapse(){//overloaded function for initial movement
+
+  //function called on inital execution of timelapse
+  void updateTimelapse(){
     Serial.println("Initial release!");
+    this->takePhoto();
     this->currentShot = 1;
     cameraMode = TIMELAPSE;
     stepperX.move(this->getDistanceToShot()); //move to next shot relative to current position
     this->timeAtLastShot = millis();
   }
+  
+  //overloaded function for initial movement, determines whether an X movement or shutter release is due
+  //function called in main event loop, takes the current time in ms and compares to the time at last shot
+  void updateTimelapse(unsigned long currentTime){
+    if(this->currentShot == this->getNumFrames()) cameraMode = MENUSTATE; //reached end of sequence, go back to menu state
+    //
+    if((currentTime - this->timeAtLastShot) >= this->getTimeToShot()*1000){//if we have reached or passed the time between shots, command next movement
+      this->takePhoto();
+      this->currentShot++;
+      stepperX.move(this->getDistanceToShot()); //move to next shot relative to current position
+      this->timeAtLastShot = millis();
+    }
+  }
 };
 
 timelapse timelapseMode;
+//--------------------------------------------------------Movement Modes--------------------------------------------------------
 
-result initTimelapse(){ //needed for callback format of menu item
+//--------------------------------------------------------Menu Callbacks--------------------------------------------------------
+
+result initTimelapse(){ //needed for callback function format
 //Callback for execute menu option
   timelapseMode.updateTimelapse();
   return proceed;
@@ -104,7 +162,6 @@ result initTimelapse(){ //needed for callback format of menu item
 
 int stepperEnable = HIGH;
 int stepperDistance = 0;
-//--------------------------------------------------------Movement Modes--------------------------------------------------------
 
 result turnOnStepper() {
   digitalWrite(X_EN, stepperEnable);
@@ -113,38 +170,22 @@ result turnOnStepper() {
   return proceed;
 }
 
-result translateStepper(){
-  Serial.println("Translating Stepper");
-  stepperX.moveTo(stepperDistance*100);
+result homeCarriage(){
+  while(digitalRead(X_LIM)){
+    stepperX.setSpeed(-3000);
+    stepperX.runSpeed();
+  }
+  stepperX.setCurrentPosition(0);
+  stepperX.moveTo(200);
   return proceed;
 }
 
-TOGGLE(stepperEnable,enableStepper,"Stepper: ",turnOnStepper,enterEvent,noStyle//,doExit,enterEvent,noStyle
-  ,VALUE("On",LOW,doNothing,noEvent)
-  ,VALUE("Off",HIGH,doNothing,noEvent)
-);
 
-MENU(timelapseMenu, "Timelapse Mode", doNothing, noEvent, wrapStyle
-  ,FIELD(timelapseMode.distanceX,"Distance","mm",0,1000,100,10,doNothing,noEvent,wrapStyle)
-  ,FIELD(timelapseMode.duration,"Duration","s",0,2000,100,10,doNothing,noEvent,wrapStyle)
-  ,FIELD(timelapseMode.clipLength,"Cliplength","s",0,100,10,1,doNothing,noEvent,wrapStyle)
-  ,FIELD(timelapseMode.framesPerSecond,"FPS","fps",0,100,10,1,doNothing,noEvent,wrapStyle)
-  ,OP("Go!",initTimelapse,enterEvent)
-  ,EXIT("<Back")
-  );
-
-MENU(mainMenu,"Main menu",doNothing,noEvent,wrapStyle
-  ,SUBMENU(enableStepper)
-  ,SUBMENU(timelapseMenu)
-);
-
-#define MAX_DEPTH 2
-
-MENU_OUTPUTS(out, MAX_DEPTH
-             , LIQUIDCRYSTAL_OUT(lcd, {0, 0, 16, 2})
-             , NONE
-            );
-NAVROOT(nav, mainMenu, MAX_DEPTH, in, out); //the navigation root object
+result translateStepper(){
+  Serial.println("Translating Stepper");
+  stepperX.move(stepperDistance*100);
+  return proceed;
+}
 
 result alert(menuOut& o, idleEvent e) {
   if (e == idling) {
@@ -169,10 +210,47 @@ result idle(menuOut& o, idleEvent e) {
   }
   return proceed;
 }
+//--------------------------------------------------------Menu Callbacks--------------------------------------------------------
+
+//------------------------------------------------------Menu Initialization-----------------------------------------------------
+//The code within this section is a collection of macros used by the arduinomenu library to minimize the amount of repeated code in the editor
+
+//Initialize a toggle option to enable the stepper
+TOGGLE(stepperEnable,enableStepper,"Stepper: ",turnOnStepper,enterEvent,noStyle//,doExit,enterEvent,noStyle
+  ,VALUE("On",LOW,doNothing,noEvent)
+  ,VALUE("Off",HIGH,doNothing,noEvent)
+);
+
+//Initialize the timelapse submenu
+MENU(timelapseMenu, "Timelapse Mode", doNothing, noEvent, wrapStyle
+  ,FIELD(timelapseMode.distanceX,"Distance","mm",0,1000,100,10,doNothing,noEvent,wrapStyle)
+  ,FIELD(timelapseMode.duration,"Duration","s",0,2000,100,10,doNothing,noEvent,wrapStyle)
+  ,FIELD(timelapseMode.clipLength,"Cliplength","s",0,100,10,1,doNothing,noEvent,wrapStyle)
+  ,FIELD(timelapseMode.framesPerSecond,"FPS","fps",0,100,10,1,doNothing,noEvent,wrapStyle)
+  ,OP("Go!",initTimelapse,enterEvent)
+  ,EXIT("<Back")
+  );
+
+//Initialize the main menu
+MENU(mainMenu,"Main menu",doNothing,noEvent,wrapStyle
+  ,SUBMENU(enableStepper)
+  ,SUBMENU(timelapseMenu)
+  ,OP("Home Carriage",homeCarriage,enterEvent)
+);
+
+#define MAX_DEPTH 2
+
+MENU_OUTPUTS(out, MAX_DEPTH
+             , LIQUIDCRYSTAL_OUT(lcd, {0, 0, 16, 2})
+             , NONE
+            );
+NAVROOT(nav, mainMenu, MAX_DEPTH, in, out); //the navigation root object
+//------------------------------------------------------Menu Initialization-----------------------------------------------------
 
 void setup() {
   pinMode(ENC_PB, INPUT_PULLUP);
-  pinMode(LEDPIN, OUTPUT);
+  pinMode(X_LIM, INPUT_PULLUP);
+  pinMode(IR_LED, OUTPUT);
   Serial.begin(115200);
   while (!Serial);
   Serial.println("Arduino Menu Library"); Serial.flush();
@@ -181,7 +259,7 @@ void setup() {
   nav.idleTask = idle; //point a function to be used when menu is suspended
   nav.showTitle = false;
   lcd.setCursor(0, 0);
-  lcd.print("Camera Ctrl");
+  lcd.print("Ardu-Slider");
   lcd.setCursor(0, 1);
   lcd.print("Stefan B.");
 
@@ -192,8 +270,8 @@ void setup() {
 }
 
 void loop() {
-  nav.poll();
-  switch(cameraMode){
+  nav.poll(); //poll the menu navigation object
+  switch(cameraMode){ //poll the appropriate function depending on the current mode
     case MENUSTATE:
     break;
     case TIMELAPSE:
@@ -202,6 +280,5 @@ void loop() {
     default:
     break;
   }
-  stepperX.run();
-  //delay(100);//simulate a delay as if other tasks are running
+  stepperX.run(); //poll the stepper motor object to see if a step is due
 }
--- PinMap.h
+++ PinMap.h
@@ -3,17 +3,19 @@
 
 const int ENC_A = 2;
 const int ENC_B = 3;
+const int ENC_PB = 4;
 const int LCD_RS = 12;
 const int LCD_E = 11;
 const int LCD_D4 = 8; 
 const int LCD_D5 = 7;
 const int LCD_D6 = 6;
 const int LCD_D7 = 5;
-const int ENC_PB = 4;
 const int X_EN = A1;
 const int X_STEP = A4;
 const int X_DIR = A5;
 const int Z_EN = A0;
 const int Z_STEP = A2;
 const int Z_DIR = A3;
+const int X_LIM = 10;
+const int IR_LED = 13;
 #endif
